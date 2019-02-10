697151f871d099ef767535f857e602d24d119cef
Fábio Araújo <fabioaraujo121@gmail.com>
2019-02-09 21:22:55 -0300

    Changing DB to PostgreSQL. Generating models based on UML.

--- mobitoken/settings.py
+++ mobitoken/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
@@ -74,12 +75,23 @@ WSGI_APPLICATION = 'mobitoken.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mobitokendb',
+        'USER': 'mobitoken',
+        'PASSWORD': 'token',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
