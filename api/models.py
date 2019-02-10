4a926cfd0cd35acb88cd619dc8fdb584862b9ba6
Fábio Araújo <fabioaraujo121@gmail.com>
2019-02-09 21:33:32 -0300

    Sending API and Readme

---
+++ api/models.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.postgres.fields import JSONField
+# To use Json Fields
+
+class Client(models.Model):
+   """A model of user"""
+   name = models.CharField(max_length=200)
+   email = models.CharField(max_length=200)
+   zoop_id = models.CharField(max_length=200)
+   phone = models.CharField(max_length=200)
+
+class Transaction(models.Model):
+   """A model of user's trasactions"""
+   buyer = models.ForeignKey("Client", related_name='%(class)s_bought')
+   seller = models.ForeignKey("Client", related_name='%(class)s_sold')
+   
+   buyer_local = JSONField()
+   seller_local = JSONField()
+
+   token = models.CharField(max_length=6)
+
+   value = models.FloatField()
+
+class AliveToken(models.Model):
+    """A model of token's that can be part of a transaction"""
+    token = models.CharField(max_length=6)
+    user = models.ForeignKey("Client")
+    beginning_at = models.DateTimeField()
+    end_at = models.DateTimeField()
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Deployment Steps
+
+1. pip install djangorestframework
+2. pip install psycopg2
+3. python manage.py migrate
